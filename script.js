93835623eeaf4908db017ac5489dd428aad0a8f3
abprasanna0816-hub <304378154+abprasanna0816-hub@users.noreply.github.com>
2026-07-13 15:54:59 +0530

    Initial S&J website

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Welcome to S&J Premium Pet Lifestyle");
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    alert("🚀 Shop Coming Soon!");
+
+});
